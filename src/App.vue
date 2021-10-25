1123e337453956d378bf572d7213569d216f31c2
Tony Trinh <tony19@gmail.com>
2021-10-25 13:25:21 -0500

    demo PropType

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <MyComponent :numbers="[1,2,3,4]"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import MyComponent from './components/MyComponent.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    MyComponent
   }
 });
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <pre>{{ numbers }}</pre>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+
+export default {
+   props: {
+      numbers: Array as PropType<Array<{}>> // eslint-disable-line @typescript-eslint/ban-types
+   }
+ }
+</script>
